bc2a627465fa24fb6ccef0101ff3e5cb56b1c566
EnriqueDaniel-waketech <ejdaniel1@my.waketech.edu>
2026-04-29 16:21:24 -0400

    Update TestLoanClassTemplate.java
    Added on lines 127-129
    double lastInterest = myLoanAmount * interest;
    	totalInterest += lastInterest;
    	monthCount++;

--- GroupProject/src/GroupProject/TestLoanClassTemplate.java
+++ GroupProject/src/GroupProject/TestLoanClassTemplate.java
@@ -124,6 +124,10 @@ public class TestLoanClassTemplate {
 		  // caculate the new month's interest based on myLoanAmount and myMonthlyInterestRate
 		  // add the interest paid this month to totalInterest
 		  // increment the month count
+		  double lastInterest = myLoanAmount * interest;
+		  totalInterest += lastInterest;
+		  monthCount++;
+
 	  }
 	  double interestSaved = l.getTotalInterest() - totalInterest;
 	  System.out.printf("Total interest $%.2f%nTotal interest saved $%.2f%nTotal months %d %n",
